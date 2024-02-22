9c895f0b3e63d514f0e9b306b5796ac180b9edc8
DaviDcD15 <156712541+DaviDcD15@users.noreply.github.com>
2024-02-22 21:24:11 +0100

    Add files via upload

---
+++ Cinema.java
@@ -0,0 +1,35 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+        Scanner sc=Scanner(System.in)
+
+     final double precioNino=4.5;
+     final double entradasAdultos =6.0
+     double totalEntradas=0
+     byte miercoles =0;
+     byte opcionMenu=0;
+     String pelicula1="Ferrari";
+     String peliculaSelecc="";
+     /*Menu de opciones*/
+        System.out.println("-----C I N E M A -----");
+        System.out.println("\T1. "pelicula1);
+        System.out.println("\t2. Priscila");
+        System.out.println("\t3 Peppa Cinema Party");
+        System.out.println("\t4 Pobres criaturas");
+        System.out.println("\t5 la  piscina");
+        System.out.println("\t6 Cazadores de demonios");
+        System.out.println("\t7 Facturar y salir");
+        System.out.println("\t7 Facturar y salir");
+        System.out.println("--------------------");
+        System.out.println("\t¿Es miercoles?");
+        System.out.println("\t1. si\t2 ");
+
+
+
+
+
+}
